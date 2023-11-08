c9118f8193bec30102dd4105392f8b617faec27d
Lucifersfier <nssrivas@gmail.com>
2023-11-08 22:06:45 +0530

    Continuing with the routing

---
+++ project_3/pkg/routes/bookstore_route.go
@@ -0,0 +1,8 @@
+package routes 
+import (
+	"github.com/gorilla/mux" // it will help to create the route
+	"github.com/Users/Nittyansh Srivastava/GOLANG_Projects/project_3/cmd/pkg/controllers"
+	
+	
+
+)
